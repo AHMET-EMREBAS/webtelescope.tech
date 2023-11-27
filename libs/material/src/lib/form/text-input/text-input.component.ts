a71a512a3fb604ac657ada88e97313034f21e266
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-11-26 19:13:01 -0600

    fonts and icons

--- libs/material/src/lib/form/text-input/text-input.component.ts
+++ libs/material/src/lib/form/text-input/text-input.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'wt-text-input',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './text-input.component.html',
   styleUrl: './text-input.component.scss',
 })
-export class TextInputComponent {}
+export class TextInputComponent {
+  @Input() label = 'Not provided';
+  @Input() errorMessage = 'Error Message';
+  @Input() hint = 'Input Hint';
+}
