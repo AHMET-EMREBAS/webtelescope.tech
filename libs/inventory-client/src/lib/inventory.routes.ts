737401ddda6c7d645a7891230975e49fc8e4dc87
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-12-01 11:57:58 -0600

    imported materials

--- libs/inventory-client/src/lib/inventory.routes.ts
+++ libs/inventory-client/src/lib/inventory.routes.ts
@@ -1,13 +1,28 @@
 import { Routes } from '@angular/router';
 import { categoryRoutes } from './category/category.routes';
-import { InventoryNavigationComponent } from './inventory-navigation.component';
 import { priceRoutes } from './price/category.routes';
-
+import {
+  AppLayoutComponent,
+  LocalStoreService,
+  provideAppName,
+  provideModuleName,
+  provideNavItems,
+} from '@webpackages/components';
 export const inventoryRoutes: Routes = [
   {
     path: '',
     title: 'Inventory',
-    component: InventoryNavigationComponent,
+    component: AppLayoutComponent,
+
+    providers: [
+      LocalStoreService,
+      provideNavItems([
+        { name: 'Category', icon: 'category', route: 'category' },
+        { name: 'Price', icon: 'money', route: 'price' },
+      ]),
+      provideAppName('Inventory'),
+      provideModuleName('Home'),
+    ],
     children: [
       {
         path: 'category',
