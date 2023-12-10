f9d11042adb9d0a572d5502c331e3eecc2ac863e
Ahmet Emrebas <aemrebas.dev@gmail.com>
2023-12-10 14:41:10 -0600

    client

--- apps/client/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-
-export const appRoutes: Route[] = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('@webpackages/inventory').then((e) => e.NavigationComponent),
-  },
-];
