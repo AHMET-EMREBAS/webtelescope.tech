7b5e490b83a30e099163c20784ac0af3f9d5aab8
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-03-28 22:02:33 -0500

    entities

---
+++ libs/entity/src/lib/order/checkout.ts
@@ -0,0 +1,29 @@
+import { CheckoutStatus, ICheckout } from '@webpackages/model';
+import {
+  BooleanColumn,
+  NumberColumn,
+  OwnerRelation,
+  TextColumn,
+  TimestampEntity,
+} from '../common';
+import { Entity } from 'typeorm';
+import { User } from '../auth';
+import { Customer } from '../customer';
+import { ShoppingCart } from './shopping-cart';
+
+@Entity()
+export class Checkout
+  extends TimestampEntity
+  implements ICheckout<User, Customer, ShoppingCart>
+{
+  @NumberColumn() total!: number;
+  @NumberColumn() subtotal!: number;
+  @NumberColumn() tax!: number;
+  @BooleanColumn() taxexempt!: boolean;
+  @TextColumn() federalTaxExemptID!: string;
+  @TextColumn() stateTaxExemptID!: string;
+  @TextColumn() status!: CheckoutStatus;
+  @OwnerRelation(ShoppingCart) shoppingCart!: ShoppingCart;
+  @OwnerRelation(Customer) customer!: Customer;
+  @OwnerRelation(User) employee!: User;
+}
