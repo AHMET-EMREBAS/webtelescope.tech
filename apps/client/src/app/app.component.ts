6e5883d549e905feabc2bf3f1e454f12e3f94a56
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-04-19 23:32:33 -0500

    locale links

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -6,6 +6,11 @@ import { RouterModule } from '@angular/router';
   imports: [RouterModule],
   selector: 'wt-root',
   template: ` <h1 i18n>Hello There!</h1>
+    <div style="display: flex; flex-direction: row ; gap: 3em;">
+      <a [routerLink]="['/en']">EN</a>
+      <a [routerLink]="['/tr']">TR</a>
+      <a [routerLink]="['/es']">ES</a>
+    </div>
     <router-outlet></router-outlet>`,
   styleUrl: './app.component.scss',
 })
