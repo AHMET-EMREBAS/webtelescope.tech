7d2dcdc46dcb6bb87151f292d8caf7f9b10dc8ed
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-04-20 19:20:09 -0500

    material storybook configuration

---
+++ libs/material/.storybook/main-theme.ts
@@ -0,0 +1,12 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    brandTitle: '@techbir/material',
+    brandUrl: 'https://github.com/ahmet-emrebas/',
+    brandImage: 'favicon.svg',
+    brandTarget: '_black',
+
+});
