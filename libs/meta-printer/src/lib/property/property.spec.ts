e534dd92247f1e3af764a324f0bac9569ea19543
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-04-27 02:11:38 -0500

    property printers implementation passed the tests.

--- libs/meta-printer/src/lib/property/property.spec.ts
+++ libs/meta-printer/src/lib/property/property.spec.ts
@@ -1,33 +1,31 @@
 import { PropertyPrinterFactory } from './property.factory';
+const Entity = new PropertyPrinterFactory().Entity();
+const Create = new PropertyPrinterFactory().Create();
+const Update = new PropertyPrinterFactory().Update();
+const Query = new PropertyPrinterFactory().Query();
+const ICreate = new PropertyPrinterFactory().ICreate();
+const IUpdate = new PropertyPrinterFactory().IUpdate();
+const View = new PropertyPrinterFactory().View();
+const IView = new PropertyPrinterFactory().IView();
+const IQuery = new PropertyPrinterFactory().IQuery();
 
 describe('PropertyPrinter', () => {
-  it('should print the property', () => {
-    const Create = new PropertyPrinterFactory().Create();
-    const Update = new PropertyPrinterFactory().Update();
-    const Query = new PropertyPrinterFactory().Query();
-    const ICreate = new PropertyPrinterFactory().ICreate();
-    const IUpdate = new PropertyPrinterFactory().IUpdate();
-    const View = new PropertyPrinterFactory().View();
-
-    expect(new Create({ name: 'some', type: 'string' }).print()).toBe(
-      `some?: string;`
-    );
-    expect(new Update({ name: 'some', type: 'string' }).print()).toBe(
-      `some?: string;`
-    );
-    expect(
-      new Query({ name: 'some', type: 'string', modelName: 'Category' }).print()
-    ).toBe(`categorySome?: string;`);
-
-    expect(new ICreate({ name: 'some', type: 'string' }).print()).toBe(
-      `some?: string;`
-    );
-
-    expect(new IUpdate({ name: 'some', type: 'string' }).print()).toBe(
-      `some?: string;`
-    );
-    expect(
-      new View({ name: 'some', type: 'string', modelName: 'Category' }).print()
-    ).toBe(`categorySome: string;`);
-  });
+  it.each`
+    expected                    | printer    | name      | type        | required | modelName
+    ${'name?: string;'}         | ${Entity}  | ${'name'} | ${'string'} | ${false} | ${'Category'}
+    ${'name?: string;'}         | ${Create}  | ${'name'} | ${'string'} | ${false} | ${'Category'}
+    ${'name?: string;'}         | ${Update}  | ${'name'} | ${'string'} | ${false} | ${'Category'}
+    ${'categoryName?: string;'} | ${Query}   | ${'name'} | ${'string'} | ${false} | ${'Category'}
+    ${'name?: string;'}         | ${ICreate} | ${'name'} | ${'string'} | ${false} | ${'Category'}
+    ${'name?: string;'}         | ${IUpdate} | ${'name'} | ${'string'} | ${false} | ${'Category'}
+    ${'categoryName!: string;'} | ${View}    | ${'name'} | ${'string'} | ${false} | ${'Category'}
+    ${'categoryName!: string;'} | ${IView}   | ${'name'} | ${'string'} | ${false} | ${'Category'}
+    ${'categoryName?: string;'} | ${IQuery}  | ${'name'} | ${'string'} | ${false} | ${'Category'}
+  `(
+    '$printer.name |  print $expected from $name, $type, $required, $modelName ',
+    ({ expected, printer, name, type, required, modelName }) => {
+      const result = new printer({ name, type, required, modelName }).print();
+      expect(result).toBe(expected);
+    }
+  );
 });
