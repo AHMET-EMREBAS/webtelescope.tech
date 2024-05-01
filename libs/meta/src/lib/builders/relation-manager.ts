c8773841cdf93eca9cc7b0a77fd8f6ef155aa38c
Ahmet Emrebas <aemrebas.dev@gmail.com>
2024-05-01 13:36:26 -0500

    done

--- libs/meta/src/lib/builders/relation-manager.ts
+++ libs/meta/src/lib/builders/relation-manager.ts
@@ -10,16 +10,29 @@ export enum BuiltinClassNames {
 export class RelationManager {
   constructor(protected readonly relationOptions: RelationOptions) {}
 
+  /**
+   * Check the relation is Many or One (isArray or not)
+   */
   protected __isArray() {
     return this.relationOptions.relationType == RelationType.Many
       ? true
       : false;
   }
 
+  /**
+   * Creates a  new model manager for the relation's model
+   */
   protected __modelManager() {
     return new ModelManager(this.relationOptions.model);
   }
 
+  /**
+   * Convert the relation options into {@link PropertyOptions}
+   * When we convert relation into property.
+   * The tye will be always an IDDto.
+   * @param options
+   * @returns
+   */
   protected __toPropertyOptions(
     options?: Partial<PropertyOptions>
   ): PropertyOptions {
@@ -38,23 +51,38 @@ export class RelationManager {
     return this.relationOptions.model.modelName;
   }
 
+  /**
+   * Convert relation options Relation column options.
+   * This method is not actually converts but pick required properties for the relation-column
+   */
   toRelationColumn(): Omit<RelationOptions, 'model'> {
-    const { relationType: type, required } = this.relationOptions;
-    return { relationType: type, required };
+    const { relationType, required } = this.relationOptions;
+    return { relationType, required };
   }
 
+  /**
+   * Convert the relation option into IEntity property option
+   */
   toIEntity(): PropertyOptions {
     return this.__toPropertyOptions({ objectType: BuiltinClassNames.IID });
   }
-
+  /**
+   * Convert the relation options into CreateDto property option
+   */
   toCreate(): PropertyOptions {
     return this.__toPropertyOptions({ objectType: BuiltinClassNames.IDDto });
   }
 
+  /**
+   * Convert the relation options into ICreate property option
+   */
   toICreate(): PropertyOptions {
     return this.__toPropertyOptions({ objectType: BuiltinClassNames.IID });
   }
 
+  /**
+   * Convert the relation options into UpdateDto property option
+   */
   toUpdate(): PropertyOptions {
     return this.__toPropertyOptions({
       objectType: BuiltinClassNames.IDDto,
@@ -62,6 +90,9 @@ export class RelationManager {
     });
   }
 
+  /**
+   * Convert the relation options into IUpdate property option
+   */
   toIUpdate(): PropertyOptions {
     return this.__toPropertyOptions({
       objectType: BuiltinClassNames.IID,
@@ -69,17 +100,22 @@ export class RelationManager {
     });
   }
 
+  /**
+   * Convert the relation options into QueryDto property option
+   */
   toQuery(): PropertyOptions[] {
     return this.__modelManager().queryProperties(this.modelName());
   }
 
+  /**
+   * Convert the relation options into IQuery property option
+   */
   toIQuery(): PropertyOptions[] {
     return this.toQuery();
   }
 
   /**
    * Return the viewable properties from the model
-   * @returns
    */
   toView(): PropertyOptions[] {
     return this.__modelManager().viewProperties();
